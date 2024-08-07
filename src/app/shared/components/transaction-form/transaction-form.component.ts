53d3aae542a439523b76b002ecd5dff9b197a903
jerome greder <j.greder@staro.ch>
2024-08-07 15:52:04 +0200

    Refactor transaction form component initialization
    Add OnInit lifecycle hook for initialization logic. Improve form controls with validators and set date to current date on form submission. Clarify service and form builder injections, and ensure form reset after submission.

--- src/app/shared/components/transaction-form/transaction-form.component.ts
+++ src/app/shared/components/transaction-form/transaction-form.component.ts
@@ -1,57 +1,65 @@
-import {Component} from '@angular/core';
-import {TransactionService} from "../../../core/services/transaction/transaction.service";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {IncomeType} from "../../../core/enums/income-type";
-import {ExpenseType} from "../../../core/enums/expense-type";
-import {Currency} from "../../../core/enums/currency";
+import { Component, OnInit } from '@angular/core';
+import { TransactionService } from "../../../core/services/transaction/transaction.service";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { IncomeType } from "../../../core/enums/income-type";
+import { ExpenseType } from "../../../core/enums/expense-type";
+import { Currency } from "../../../core/enums/currency";
 
 @Component({
   selector: 'app-transaction-form',
   templateUrl: './transaction-form.component.html',
-  styleUrl: './transaction-form.component.scss'
+  styleUrls: ['./transaction-form.component.scss']
 })
-export class TransactionFormComponent {
+export class TransactionFormComponent implements OnInit {
 
   incomeTypes = Object.values(IncomeType);
-  expenseTypes = Object.values(ExpenseType)
+  expenseTypes = Object.values(ExpenseType);
   currencies = Object.values(Currency);
   isIncome = true;
   isRecurring = false;
+  transactionForm: FormGroup;
 
   constructor(
-    public transactionService: TransactionService,  // Assuming TransactionService is injected here
-    public _fb: FormBuilder
-  ) {
+    private transactionService: TransactionService,
+    private _fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.transactionForm = this._fb.group({
+      type: ['income'],
+      amount: [null, [Validators.required, Validators.min(0)]],
+      category: [null, Validators.required],
+      recurring: [false],
+      currency: [null, Validators.required],
+      date: [null],
+    });
   }
 
-  transactionForm = this._fb.group({
-    type: ['income'],
-    amount: [null],
-    category: [null],
-    recurring: [false],
-    currency: [null],
-    date: [new Date()]
-  })
-
-  onSubmit() {
-    if (this.transactionForm.invalid) return console.log('Form is invalid');
+  onSubmit(): void {
+    if (this.transactionForm.invalid) {
+      console.error('Form is invalid');
+      return;
+    }
+    this.transactionForm.patchValue({ date: new Date(this.transactionForm.value.date) });
 
     console.log(this.transactionForm.value);
+    this.transactionForm.reset();
   }
 
-  switchType() {
-    this.transactionForm.patchValue({type: this.transactionForm.value.type === 'income' ? 'expense' : 'income'});
-    this.isIncome =!this.isIncome;
+  switchType(): void {
+    const newType = this.transactionForm.value.type === 'income' ? 'expense' : 'income';
+    this.transactionForm.patchValue({ type: newType });
+    this.isIncome = !this.isIncome;
   }
 
-  setToRecurring() {
-    this.transactionForm.patchValue({recurring: !this.transactionForm.value.recurring});
+  setToRecurring(): void {
     this.isRecurring = !this.isRecurring;
+    this.transactionForm.patchValue({ recurring: this.isRecurring });
+
     if (this.isRecurring) {
-      (this.transactionForm as FormGroup).addControl('recurringDate', this._fb.control(null, Validators.required));
+      this.transactionForm.addControl('recurringDate', this._fb.control(null, Validators.required));
     } else {
-      (this.transactionForm as FormGroup).removeControl('recurringDate');
+      this.transactionForm.removeControl('recurringDate');
     }
   }
-
 }
